6d31996bb69c90b35907348e55d6fc02c67ecc39
Varun kumar Tiwary <varunkumartiwary2@gmail.com>
2025-01-01 15:42:12 +0530

    Added Button and CallToAction Component on the Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import { Container } from "./styles";
 import Header from "../../components/Header";
+import CallToAction from "../../components/CallToAction";
 
 const Home: FC = (): JSX.Element => {
   return (
     <Container>
       <Header />
+      <CallToAction />
     </Container>
   );
 };
